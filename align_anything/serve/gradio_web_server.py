1aeec717352ec34182bfc0648f89d3acd3a7ea70
yongzhemiaolegemi <2200017735@stu.pku.edu.cn>
2024-07-18 21:27:15 +0800

    match jiayi code

--- align_anything/serve/gradio_web_server.py
+++ align_anything/serve/gradio_web_server.py
@@ -39,7 +39,6 @@ server_error_msg = "**NETWORK ERROR DUE TO HIGH TRAFFIC. PLEASE REGENERATE OR RE
 moderation_msg = "YOUR INPUT VIOLATES OUR CONTENT MODERATION GUIDELINES. PLEASE TRY AGAIN."
 
 LOGDIR = "."
-models = {}
 
 @dataclasses.dataclass
 class Conversation:
