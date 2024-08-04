a78fb5613f8311294d9fdbfc0dde35544ab6562d
TungHoi <hdh1207726295@gmail.com>
2024-08-04 16:29:17 +0800

    fix bugs

--- align_anything/evaluation/begin.sh
+++ align_anything/evaluation/begin.sh
@@ -2,6 +2,6 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 python __main__.py \
-    -b BBH \
-    --output_dir /aifs4su/yaodong/donghai/align-anything/align_anything/evaluation/meta_test_output/bbh \
-    -g vllm
+    -b MME \
+    --output_dir /aifs4su/yaodong/donghai/align-anything/align_anything/evaluation/meta_test_output/mme \
+    -g deepspeed
