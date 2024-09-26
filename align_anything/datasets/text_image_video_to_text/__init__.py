c8f2df0e58f74cb73712af6f33bee07dc6a9ac20
htlou <lht_pku@stu.pku.edu.cn>
2024-09-27 02:47:51 +0800

    correct a typo

--- align_anything/datasets/text_image_video_to_text/__init__.py
+++ align_anything/datasets/text_image_video_to_text/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Dataset classes for text to text training."""
+"""Dataset classes for text, image and video to text training."""
 
 
 from align_anything.datasets.text_image_video_to_text.supervised import *
