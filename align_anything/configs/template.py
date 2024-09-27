d5b9bd12f3963a812c70fc10731369b1364ac70d
htlou <lht_pku@stu.pku.edu.cn>
2024-09-27 16:05:43 +0800

    Change template name

--- align_anything/configs/template.py
+++ align_anything/configs/template.py
@@ -1158,8 +1158,8 @@ class SOMOS:
     def check_equal(self, raw_sample: dict[str, Any]) -> bool:
         return False
 
-@register_template('TV2T')
-class TV2T:
+@register_template('Qwen2-VL')
+class QWEN2VL:
     system_prompt: str = '<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n'
     user_prompt: str = '<|im_start|>user\n{input}<|im_end|>\n'
     assistant_prompt: str = '<|im_start|>assistant\n{output}'
@@ -1194,8 +1194,8 @@ class TV2T:
         }
         return return_dict
 
-@register_template('TV2T_preference')
-class TV2T_PREF:
+@register_template('Qwen2VL_preference')
+class QWEN2VL_PREF:
     system_prompt: str = '<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n'
     user_prompt: str = '<|im_start|>user\n{input}<|im_end|>\n'
     assistant_prompt: str = '<|im_start|>assistant\n{output}'
