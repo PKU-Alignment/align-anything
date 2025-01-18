8a374fdeb93bbf626d9377e74975f3a44f8125de
Jiayi Zhou <108712610+Gaiejj@users.noreply.github.com>
2025-01-18 21:32:29 +0800

    Update minicpmo_sft.sh

--- scripts/minicpmo_sft.sh
+++ scripts/minicpmo_sft.sh
@@ -1,7 +1,25 @@
+#!/usr/bin/env bash
+#
+# Copyright 2025 PKU-Alignment Team. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+
 MODEL_NAME_OR_PATH="openbmb/MiniCPM-o-2_6" # model path
 export MODEL_NAME_OR_PATH
 
-TRAIN_DATASETS="PKU-Alignment/Align-Anything-TI2T-Instruction-100K" # dataset path\
+TRAIN_DATASETS="PKU-Alignment/Align-Anything-TI2T-Instruction-100K" # dataset path
 TRAIN_TEMPLATE="AA_TI2T" # dataset template
 TRAIN_SPLIT="train" # split the dataset
 
