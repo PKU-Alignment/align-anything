3e31ba861b97d64047f945be925d076136533044
pcwen <pcwen@outlook.com>
2025-03-15 14:46:43 +0800

    Update slurm_llava_dpo.sh

--- scripts/slurm_llava_dpo.sh
+++ scripts/slurm_llava_dpo.sh
@@ -7,10 +7,6 @@
 #SBATCH --nodes=1                           
 #SBATCH --gres=gpu:8                  
 
-# follow the same steps to activate the conda environment
-conda activate align-anything
-export PYTHONPATH=/path/to/your/align-anything/
-cd /path/to/your/align-anything/scripts
 
 # Run the script
 srun  llava_dpo.sh
